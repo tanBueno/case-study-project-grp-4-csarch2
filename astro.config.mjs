32db3f782a98a70e734ef6475871f77e20c5d612
Kane Joshua Ibañez <kane_ibanez@dlsu.edu.ph>
2026-07-07 23:58:00 +0800

    Refactor to use template HomepageLayout with timeline redirect

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,10 +3,11 @@ import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
 import mdx from '@astrojs/mdx';
 import tailwindcss from '@tailwindcss/vite';
+import icon from 'astro-icon';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react(), mdx()],
+  integrations: [react(), mdx(), icon()],
   vite: {
     plugins: [tailwindcss()],
   },
